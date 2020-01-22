c3c5ecfcaa7ce5c766fb7dea8308a3de8a130785
Joan Marín <36265613+X0KA@users.noreply.github.com>
2020-01-22 05:54:38 +0100

    Main menu UI created

---
+++ Lost Signal Project/Assets/UI/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Blocking_Scene");
+    }
+
+   public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
