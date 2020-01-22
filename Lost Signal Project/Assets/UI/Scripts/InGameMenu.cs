2ddf5fb2297a9472770423f6dedb415e6d9fe4b0
Joan Marín <36265613+X0KA@users.noreply.github.com>
2020-01-22 07:00:11 +0100

    UI Screen workflow added

---
+++ Lost Signal Project/Assets/UI/Scripts/InGameMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    public GameObject IngameMenu;
+    private bool menuEnabled = false;
+
+
+    void Update()
+    {
+        menuEnabled = IngameMenu.active;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            IngameMenu.SetActive(!menuEnabled);
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene("Blocking_Scene");
+    }
+}
